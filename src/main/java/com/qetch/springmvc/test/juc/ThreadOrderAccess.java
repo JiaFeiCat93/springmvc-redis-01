b4957a349ca0625603bd223629419fa33b39f1d7
JiaFeiCat93 <37857620+JiaFeiCat93@users.noreply.github.com>
2019-04-10 08:34:31 +0800

    Merge branch 'master' of https://github.com/JiaFeiCat93/springmvc-redis-01
                           # Conflicts:
                           #	.idea/workspace.xml

---
+++ src/main/java/com/qetch/springmvc/test/juc/ThreadOrderAccess.java
@@ -0,0 +1,98 @@
+package com.qetch.springmvc.test.juc;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class ShareResource {
+    private int number = 1; // 1->AA 2->BB 3->CC
+    private Lock lock = new ReentrantLock();
+    Condition c1 = lock.newCondition();
+    Condition c2 = lock.newCondition();
+    Condition c3 = lock.newCondition();
+
+    public void print5() {
+        lock.lock();
+        try {
+            while (number != 1) { // 当前不是AA线程，所以等待
+                c1.await();
+            }
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 2; // AA线程执行完毕后，立即修改标志位
+            c2.signal(); // AA线程执行完毕后，只通知BB线程
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print10() {
+        lock.lock();
+        try {
+            while (number != 2) { // 当前不是AA线程，所以等待
+                c2.await();
+            }
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 3; // AA线程执行完毕后，立即修改标志位
+            c3.signal(); // AA线程执行完毕后，只通知BB线程
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print15() {
+        lock.lock();
+        try {
+            while (number != 3) { // 当前不是AA线程，所以等待
+                c3.await();
+            }
+            for (int i = 1; i <= 15; i++) {
+                System.out.println(Thread.currentThread().getName() + "\t" + i);
+            }
+            number = 1; // AA线程执行完毕后，立即修改标志位
+            c1.signal(); // AA线程执行完毕后，只通知BB线程
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
+
+/**
+ * 多线程之间按顺序调用，实现A->B->C
+ * 三个线程启动，要求如下：
+ * AA打印5次，BB打印10次，CC打印15次
+ * 接着
+ * AA打印5次，BB打印10次，CC打印15次
+ * ......来10轮
+ */
+public class ThreadOrderAccess {
+
+    public static void main(String[] args) {
+        ShareResource sr = new ShareResource();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                sr.print5();
+            }
+        }, "AA").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                sr.print10();
+            }
+        }, "BB").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                sr.print15();
+            }
+        }, "CC").start();
+    }
+}
