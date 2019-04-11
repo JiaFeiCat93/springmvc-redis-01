2c27b05a4e949585efc3c34280b7c86c85d235d4
JiaFeiCat93 <1046709362@qq.com>
2019-04-11 18:34:56 +0800

    add lock_8 test itc 20190411

--- src/main/java/com/qetch/springmvc/test/juc/shangguigu/Lock_8.java
+++ src/main/java/com/qetch/springmvc/test/juc/shangguigu/Lock_8.java
@@ -1,5 +1,6 @@
 package com.qetch.springmvc.test.juc.shangguigu;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 class Phone {
@@ -9,6 +10,18 @@ class Phone {
     public void sendSMS() {
         lock.lock();
         try {
+            System.out.println("***************sendSMS()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void sendSMS_sleep_4s() {
+        lock.lock();
+        try {
+            TimeUnit.SECONDS.sleep(4);
             System.out.println("***************sendSMS()");
         } catch (Exception e) {
             e.printStackTrace();
@@ -27,17 +40,101 @@ class Phone {
             lock.unlock();
         }
     }
+
+    public void openPC() {
+        System.out.println("***************openPC()");
+    }
 }
 
 /**
  * 1    标准访问，请问先打印sms还是email
+ * 2    sendSMS()睡眠4秒钟，请问先打印sms还是email
+ * 3    新增普通方法openPC()，请问先打印sms还是email
+ * 4    有两部手机，请问先打印sms还是email
  */
 public class Lock_8 {
 
     public static void main(String[] args) {
         Phone phone = new Phone();
+        Phone phone2 = new Phone();
 
-        test1(phone);
+        //test1(phone);
+
+        //test2(phone);
+
+        //test3(phone);
+
+        //test4(phone, phone2);
+
+    }
+
+    private static void test4(Phone phone, Phone phone2) {
+        new Thread(() -> {
+            phone.sendSMS_sleep_4s();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone2.sendEMail();
+        }, "BB").start();
+
+        /**
+         * 运行结果：
+         * ***************sendEMail()
+         * （等待4秒）
+         * ***************sendSMS()
+         */
+    }
+
+    private static void test3(Phone phone) {
+        new Thread(() -> {
+            phone.sendSMS_sleep_4s();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone.openPC();
+        }, "BB").start();
+
+        /**
+         * 运行结果：
+         * ***************openPC()
+         * （等待4秒）
+         * ***************sendSMS()
+         */
+    }
+
+    private static void test2(Phone phone) {
+        new Thread(() -> {
+            phone.sendSMS_sleep_4s();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone.sendEMail();
+        }, "BB").start();
+
+        /**
+         * 运行结果：
+         * （等待4秒）
+         * ***************sendSMS()
+         * ***************sendEMail()
+         */
     }
 
     private static void test1(Phone phone) {
