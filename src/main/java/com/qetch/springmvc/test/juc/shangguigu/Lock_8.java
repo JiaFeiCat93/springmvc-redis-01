de62c3914ab3a2a13b40408eecd56b6a569a3e7c
JiaFeiCat93 <37857620+JiaFeiCat93@users.noreply.github.com>
2019-04-11 08:35:15 +0800

    add lock_8 test ah 20190411

--- src/main/java/com/qetch/springmvc/test/juc/shangguigu/Lock_8.java
+++ src/main/java/com/qetch/springmvc/test/juc/shangguigu/Lock_8.java
@@ -1,19 +1,66 @@
 package com.qetch.springmvc.test.juc.shangguigu;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 class Phone {
 
+    private ReentrantLock lock = new ReentrantLock();
+
     public void sendSMS() {
-        System.out.println("***************sendSMS()");
+        lock.lock();
+        try {
+            System.out.println("***************sendSMS()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void sendEMail() {
-        System.out.println("***************sendEMail()");
+        lock.lock();
+        try {
+            System.out.println("***************sendEMail()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
     }
 }
 
+/**
+ * 1    标准访问，请问先打印sms还是email
+ */
 public class Lock_8 {
 
     public static void main(String[] args) {
+        Phone phone = new Phone();
 
+        test1(phone);
+    }
+
+    private static void test1(Phone phone) {
+        new Thread(() -> {
+            phone.sendSMS();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone.sendEMail();
+        }, "BB").start();
+
+        /**
+         * 运行结果：
+         * ***************sendSMS()
+         * ***************sendEMail()
+         *
+         * 结论：
+         */
     }
 }
