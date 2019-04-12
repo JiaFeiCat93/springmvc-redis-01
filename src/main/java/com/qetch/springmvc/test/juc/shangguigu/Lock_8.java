f5d59d5f7a2018a211e61f9a6bae9023abca4f43
JiaFeiCat93 <37857620+JiaFeiCat93@users.noreply.github.com>
2019-04-12 08:40:42 +0800

    add lock_8 test ah 20190412

--- src/main/java/com/qetch/springmvc/test/juc/shangguigu/Lock_8.java
+++ src/main/java/com/qetch/springmvc/test/juc/shangguigu/Lock_8.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 class Phone {
 
-    private ReentrantLock lock = new ReentrantLock();
+    private static ReentrantLock lock = new ReentrantLock();
 
     public void sendSMS() {
         lock.lock();
@@ -30,7 +30,30 @@ class Phone {
         }
     }
 
+    public static void sendSMS_sleep_4s_static() {
+        lock.lock();
+        try {
+            TimeUnit.SECONDS.sleep(4);
+            System.out.println("***************sendSMS()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
     public void sendEMail() {
+        lock.lock();
+        try {
+            System.out.println("***************sendEMail()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void sendEMail_static() {
         lock.lock();
         try {
             System.out.println("***************sendEMail()");
@@ -51,6 +74,10 @@ class Phone {
  * 2    sendSMS()睡眠4秒钟，请问先打印sms还是email
  * 3    新增普通方法openPC()，请问先打印sms还是email
  * 4    有两部手机，请问先打印sms还是email
+ * 5    两个静态同步方法，有一部手机，请问先打印sms还是email
+ * 6    两个静态同步方法，有两部手机，请问先打印sms还是email
+ * 7    一个静态同步方法，一个普通同步方法，同一部手机，请问先打印sms还是email
+ * 8    一个静态同步方法，一个普通同步方法，两部手机，请问先打印sms还是email
  */
 public class Lock_8 {
 
@@ -66,6 +93,88 @@ public class Lock_8 {
 
         //test4(phone, phone2);
 
+        //test5(phone);
+
+        //test6(phone, phone2);
+
+        //test7(phone);
+
+        new Thread(() -> {
+            phone.sendSMS_sleep_4s_static();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone2.sendEMail();
+        }, "BB").start();
+    }
+
+    private static void test7(Phone phone) {
+        new Thread(() -> {
+            phone.sendSMS_sleep_4s_static();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone.sendEMail();
+        }, "BB").start();
+    }
+
+    private static void test6(Phone phone, Phone phone2) {
+        new Thread(() -> {
+            phone.sendSMS_sleep_4s_static();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone2.sendEMail_static();
+        }, "BB").start();
+
+        /**
+         * 运行结果：
+         * （等待4秒）
+         * ***************sendSMS()
+         * ***************sendEMail()
+         */
+    }
+
+    private static void test5(Phone phone) {
+        new Thread(() -> {
+            phone.sendSMS_sleep_4s_static();
+        }, "AA").start();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            phone.sendEMail_static();
+        }, "BB").start();
+
+        /**
+         * 运行结果：
+         * （等待4秒）
+         * ***************sendSMS()
+         * ***************sendEMail()
+         *
+         */
     }
 
     private static void test4(Phone phone, Phone phone2) {
